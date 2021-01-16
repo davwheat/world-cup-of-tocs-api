2732a129d2a369107beb74891db16776be6e5286
David Wheatley <hi@davwheat.dev>
2021-01-16 09:28:40 +0000

    Fix restarting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'WCOTOCs API',
+      name: 'WCOTOCs API v2',
       script: 'index.js',
       watch: true,
       watch_delay: 1000,
-      ignore_watch: ['node_modules', 'data', './cup.json', '.git'],
-      args: ['--color', '--time'],
+      ignore_watch: ['node_modules', 'data', './cup.json', '.git', 'access.log'],
+      args: ['--color', '--time', '--merge-logs'],
     },
   ],
 }

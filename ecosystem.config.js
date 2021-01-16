27d6062702a31dd5496f40b07c4c533e39c56574
David Wheatley <hi@davwheat.dev>
2021-01-16 11:30:05 +0000

    Add combined log file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,10 @@ module.exports = {
     {
       name: 'WCOTOCs API v2',
       script: 'index.js',
+      log_file: 'combined_pm2.log',
       watch: true,
       watch_delay: 1000,
-      ignore_watch: ['node_modules', 'data', './cup.json', '.git', 'access.log'],
+      ignore_watch: ['node_modules', 'data', './cup.json', '.git', '*.log'],
       args: ['--color', '--time'],
     },
   ],

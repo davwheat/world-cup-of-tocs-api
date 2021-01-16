984324aab83a310555e90fd95238bdac9a503f2e
David Wheatley <hi@davwheat.dev>
2021-01-16 13:25:02 +0000

    Enable caching

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Methods', 'GET')
   res.header('Access-Control-Max-Age', 86400)
-  // res.header('Cache-Control', `public, max-age=60, stale-if-error=600, stale-while-revalidate=120`)
-  res.header('Cache-Control', `no-store, max-age=0`)
+  res.header('Cache-Control', `public, max-age=60, stale-if-error=600, stale-while-revalidate=120`)
   next()
 })
 
